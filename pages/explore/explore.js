80c5ddc0f6d597ef3330a0e4ada2bc2164f05634
liaolingjia <353050104@qq.com>
2019-08-25 21:00:56 +0800

    fix

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -5,62 +5,120 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        datas: [{
+            type: "",
+            title: "经典色卡",
+            data: [{
+                "color3": "177E89",
+                "color0": "FFC857",
+                "color1": "DB3A34",
+                "color2": "084C61",
+                "index": "0"
+            }, {
+                "color3": "1F2B40",
+                "color0": "2E7866",
+                "color1": "F2C640",
+                "color2": "3F485B",
+                "index": "1"
+            }, {
+                "color3": "CFDEE7",
+                "color0": "4472CA",
+                "color1": "5E7CE2",
+                "color2": "92B4F4",
+                "index": "2"
+            }, {
+                "color3": "DFDCE3",
+                "color0": "4ABDAC",
+                "color1": "FC4A1A",
+                "color2": "F7B733",
+                "index": "3"
+            }, {
+                "color3": "526187",
+                "color0": "4D2831",
+                "color1": "B50B1E",
+                "color2": "0D1130",
+                "index": "4"
+            }, {
+                "color3": "9dd7d5",
+                "color0": "1f9792",
+                "color1": "505050",
+                "color2": "fff4c5",
+                "index": "5"
+            }, {
+                "color3": "e62739",
+                "color0": "6ed3cf",
+                "color1": "9068be",
+                "color2": "e1e8f0",
+                "index": "6"
+            }, {
+                "color3": "3A4457",
+                "color0": "AE8D65",
+                "color1": "C29D73",
+                "color2": "717A84",
+                "index": "7"
+            }, {
+                "color3": "F0E5D0",
+                "color0": "3B3115",
+                "color1": "EBAF60",
+                "color2": "205C55",
+                "index": "8"
+            }]
+        }]
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function(options) {
 
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
+    onReady: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
+    onShow: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {
+    onHide: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {
+    onUnload: function() {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
+    onPullDownRefresh: function() {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
+    onReachBottom: function() {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage: function() {
 
     }
 })
